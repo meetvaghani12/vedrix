6042cf178fd0ea00feff8d07a6f67722ca7d6d31
Meet Vaghani <meetvaghani1238@gmail.com>
2025-07-02 22:38:53 +0530

    updated again

--- backend/turnitin_backend/settings.py
+++ backend/turnitin_backend/settings.py
@@ -28,8 +28,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-@&kv*fs(0*k1a*a%ru3k(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=Csv())
-
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1,vedrix.onrender.com,vedrix.vercel.app', cast=Csv())
 
 # Application definition
 
